e33ed5837b704bde999c0061cdc68238693ac460
Phemy78 <126839684+Phemy78@users.noreply.github.com>
2023-04-17 21:43:49 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my2023-aws-tf-state-bucket"
-    key = "main"
+    key = "master"
     region = "us-east-1"
     dynamodb_table = "my2023-dynamo-db-table"
   }
