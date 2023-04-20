4fb219eabcae7e06e89b2a3319330ed1441fbd7d
Phemy78 <126839684+Phemy78@users.noreply.github.com>
2023-04-19 21:16:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "master" {
+resource "aws_vpc" "main" {
   cidr_block = "172.16.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "master"
+    Name = "main"
   }
 }
 
